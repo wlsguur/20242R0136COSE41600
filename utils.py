8d02b5180dbb50f2af8247c106fe022c266d64d2
wlsguur <wlsgur011018@gmail.com>
2024-11-23 17:22:59 +0900

    Feat: Add save trajectory and video

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import open3d as o3d
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import cv2
+import os
 
 def preprocess_pcd(pcd):
     downsample_pcd = pcd.voxel_down_sample(voxel_size=0.2)
@@ -28,20 +30,48 @@ def DBSCAN(pcd):
 
     return pcd, labels
 
-def visualize_pcd(pcd, bounding_boxes=None, window_name="Filtered Clusters and Bounding Boxes", point_size=1.0):
-    vis = o3d.visualization.Visualizer()
-    vis.create_window(window_name=window_name)
-    vis.add_geometry(pcd)
-    for bbox in bounding_boxes:
-        vis.add_geometry(bbox)
-    vis.get_render_option().point_size = point_size
-    vis.run()
-    vis.destroy_window()
+def visualize_pcd_trajectory(pcd_list):
+    o3d.visualization.draw_geometries(pcd_list, window_name="Pedestrian trajectory", width=1600, height=1200)
 
-def visualize_pcd_sequence(pcd_sequence, bbox_sequence, window_name="PCD Sequence Visualization", point_size=1.0, fps=5):
+def save_pcd_trajectory(pcd_list,
+                        save_dir="result/", 
+                        window_name="Pedestrian trajectory",
+                        point_size=2.0):
+                                
     vis = o3d.visualization.Visualizer()
-    vis.create_window(window_name=window_name)
+    vis.create_window(window_name=window_name, width=1600, height=1200)
     vis.get_render_option().point_size = point_size
+
+    for geom in pcd_list:
+        vis.add_geometry(geom)
+    
+    vis.poll_events()
+    vis.update_renderer()
+
+    save_path = os.path.join(save_dir, "trajectory.png")
+    vis.capture_screen_image(save_path, do_render=True)
+    vis.destroy_window()
+    print(f"Image saved to {save_path}")
+
+
+def visualize_pcd_sequence(pcd_sequence,
+                           bbox_sequence,
+                           save_video=False,
+                           save_dir="result/",
+                           window_name="Pedestrian with bbox video",
+                           point_size=1.0,
+                           fps=5):
+    
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(window_name=window_name, width=1600, height=1200)
+    vis.get_render_option().point_size = point_size
+
+    if save_video:
+        frame_width = 1600
+        frame_height = 1200
+        fourcc = cv2.VideoWriter_fourcc(*'avc1')
+        save_path = os.path.join(save_dir, "video.mp4")
+        video_writer = cv2.VideoWriter(save_path, fourcc, fps, (frame_width, frame_height))
 
     for i in range(len(pcd_sequence)):
         vis.clear_geometries()
@@ -49,9 +79,20 @@ def visualize_pcd_sequence(pcd_sequence, bbox_sequence, window_name="PCD Sequenc
         if bbox_sequence[i] is not None:
             for bbox in bbox_sequence[i]:
                 vis.add_geometry(bbox)
+
         vis.poll_events()
         vis.update_renderer()
+
+        if save_video:
+            img = vis.capture_screen_float_buffer(do_render=True)
+            img = (255 * np.asarray(img)).astype(np.uint8)
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            video_writer.write(img)
 
         time.sleep(1.0 / fps)
 
     vis.destroy_window()
+
+    if save_video:
+        video_writer.release()
+        print(f"Video saved to {save_path}")
